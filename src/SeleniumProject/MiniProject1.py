914b247771abefd05e2e833315bcb97196caff0c
Gaurav <gaurav.nigam2501@gmail.com>
2024-05-26 20:49:37 +0530

    test

--- src/SeleniumProject/MiniProject1.py
+++ src/SeleniumProject/MiniProject1.py
@@ -1,40 +1,42 @@
-#open the url - https://katalon-demo-cura.herokuapp.com/
-#click on the make appoinment button
-#verify that url changes - assert
-#time.sleep(3)
-#enter the username, password
-#next page verify the current url
-#make appoinment text on the web page
-
 import time
+import pytest
+import allure
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import time
-import allure
-from allure_commons.type import AttachmentType
+from allure_commons.types import AttachmentType
 
-@allure.title("Cura")
-@allure.description("Make Appointment")
-def test_mini_project():
+
+# Selenium 4
+
+@pytest.mark.smoke
+@allure.title("Verify that Login is working in Cura website")
+@allure.description("TC#1 - Simple Login check on CURA katalong Website.")
+def test_vwologin_negative_tc():
     driver = webdriver.Chrome()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
-    driver.maximize_window()
-    make_element = driver.find_element(By.ID,"btn-make-appointment")
-    make_element.click()
-    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login"
-    time.sleep()
-    user_element = driver.find_element(By.ID,"txt-username")
-    user_element.send_keys("John Doe")
-    user_element = driver.find_element(By.ID,value="txt-password")
-    user_element.send_keys("ThisIsNotAPassword")
-    log_in = driver.find_element(By.ID,"btn-login")
-    log_in = click()
-    time.sleep(3)
-    verify_text = driver.find_element(By.XPATH, "//div/h2")
-    print(verify_text)  
-    assert verify_text.text == "Make Appointment"
-    allure.attach(driver.get_screenshot_as_png(), name="Screenshot", attachment_type=AttachmentType.PNG)
 
+    make_appoitnment_btn = driver.find_element(By.LINK_TEXT, "Make Appointment")
+    make_appoitnment_btn.click()
 
+    allure.attach(driver.get_screenshot_as_png(), name="login-screenshot", attachment_type=AttachmentType.PNG)
 
+    print(driver.current_url)
 
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/profile.php#login", "Assertion Fail Message #1 - Error Matching the URLs"
+
+    username = driver.find_element(By.NAME, "username")
+    username.send_keys("John Doe")
+
+    password = driver.find_element(By.ID, "txt-password")
+    password.send_keys("ThisIsNotAPassword")
+
+    submit_btn = driver.find_element(By.ID, "btn-login")
+    submit_btn.click()
+
+    allure.attach(driver.get_screenshot_as_png(), name="appointment-screenshot", attachment_type=AttachmentType.PNG)
+
+    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/#appointment", "Assertion Fail Message #2 - Error wrong URL(appointment)"
+
+    # // a[ @ id = 'btn-make-appointment'] - 1 unique elemnt
+
+    driver.quit()
